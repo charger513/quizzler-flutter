7d1b05308b01644812110fc304e67ab6895779f2
Emmanuel Chargoy <charger513@hotmail.com>
2020-04-01 22:26:29 -0600

    Encapsulation in action

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'package:quizzler/quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,26 +27,8 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
-  // List<String> questions = [
-  //   'You can lead a cow down stairs but not up stairs.',
-  //   'Approximately one quarter of human bones are in the feet.',
-  //   'A slug\'s blood is green.'
-  // ];
 
-  // List<bool> answers = [
-  //   false,
-  //   true,
-  //   true
-  // ];
-
-  List<Question> questions = [
-    Question(questionText: 'You can lead a cow down stairs but not up stairs.', questionAnswer: false ),
-    Question(questionText: 'Approximately one quarter of human bones are in the feet.', questionAnswer: true ),
-    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true )
-  ];
-
-
-  int questionNumber = 0;
+  QuizBrain quizBrain = QuizBrain();
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +42,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -86,7 +68,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked true.
 
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = quizBrain.getQuestionAnswer();
 
                 if(correctAnswer) {
                   scoreKeeper.add(
@@ -99,7 +81,7 @@ class _QuizPageState extends State<QuizPage> {
                 }
 
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
             ),
@@ -119,7 +101,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 // The user picked false.
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = quizBrain.getQuestionAnswer();
 
                 if(!correctAnswer) {
                   scoreKeeper.add(
@@ -132,7 +114,7 @@ class _QuizPageState extends State<QuizPage> {
                 }
 
                 setState(() {
-                  questionNumber++;
+                  quizBrain.nextQuestion();
                 });
               },
             ),
